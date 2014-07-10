ac46485cd2a77aaa3f9fe79279d459d52516386d
Painted <will.allen@paintedltd.co.uk>
2014-07-10 16:06:16 +0100

    created a form for posts

---
+++ spec/features/posts_feature_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'posts listing page' do 
+	
+	context 'no posts' do 
+		it 'tells me that there are no posts' do 
+			visit '/posts'
+			expect(page).to have_content 'No posts yet'
+		end
+	end
+
+	context 'with posts' do 
+		before do 
+			Post.create(title: 'Happy', description: 'Photo of happy kids')
+		end
+		it 'should show the post' do 
+			visit '/posts'
+			expect(page).to have_content 'Happy'
+			expect(page).to have_content 'Photo of happy kids'
+		end
+	end
+end
+
+describe 'creating posts' do
+  it 'adds the post to the homepage' do
+    visit '/posts/new'
+    fill_in 'Title', with: 'My new post'
+    fill_in 'Description', with: 'Lorem ipsum'
+    click_button 'Post it!'
+
+    expect(current_path).to eq posts_path
+    expect(page).to have_content 'My new post'
+  end
+end

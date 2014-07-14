81252a587c6f4d18ed287989db4b7e55a74dd1c8
Painted <will.allen@paintedltd.co.uk>
2014-07-14 11:24:05 +0100

    added devise for user login, etc

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 describe 'posts listing page' do 
 	
 	context 'no posts' do 
+
+		it 'displays a prompt to create posts' do 
+			visit '/posts'
+
+			expect(page).to have_link 'New post'
+		end
+		
 		it 'tells me that there are no posts' do 
 			visit '/posts'
 			expect(page).to have_content 'No posts yet'
@@ -22,26 +29,44 @@ describe 'posts listing page' do
 end
 
 describe 'creating posts' do
-  it 'adds the post to the homepage' do
-    visit '/posts/new'
-    fill_in 'Title', with: 'My new post'
-    fill_in 'Description', with: 'Lorem ipsum'
-    click_button 'Post it!'
 
-    expect(current_path).to eq posts_path
-    expect(page).to have_content 'My new post'
-    expect(page).not_to have_css 'img.uploaded-pic'
-  end
+	context 'when logged out' do 
+		it 'is impossible' do 
+			visit '/posts'
+			click_link 'New post'
+			expect(page).to have_content 'Sign in'
+			expect(page).not_to have_field 'Title'
+		end
+	end
 
-  it 'can add a photo to our posts' do
-    visit '/posts/new'
-    fill_in 'Title', with: 'My new post'
-    fill_in 'Description', with: 'Lorem ipsum'
-    attach_file 'Image', Rails.root.join('spec/images/IMG_1691.jpg')
+	context 'logged in'	do 
 
-    click_button 'Post it!'
+		before do 
+			will = User.create(email: 'w@will.com', password: '12345678', password_confirmation: '12345678')
+			login_as will
+		end
 
-    expect(current_path).to eq posts_path
-    expect(page).to have_css 'img.uploaded-pic'
-  end
+		it 'adds the post to the homepage' do
+			visit '/posts/new'
+		    fill_in 'Title', with: 'My new post'
+		    fill_in 'Description', with: 'Lorem ipsum'
+		    click_button 'Post it!'
+
+		    expect(current_path).to eq posts_path
+		    expect(page).to have_content 'My new post'
+		    expect(page).not_to have_css 'img.uploaded-pic'
+		end
+
+		it 'can add a photo to our posts' do
+		    visit '/posts/new'
+		    fill_in 'Title', with: 'My new post'
+		    fill_in 'Description', with: 'Lorem ipsum'
+		    attach_file 'Image', Rails.root.join('spec/images/IMG_1691.jpg')
+
+		    click_button 'Post it!'
+
+		    expect(current_path).to eq posts_path
+		    expect(page).to have_css 'img.uploaded-pic'
+		end
+	end
 end

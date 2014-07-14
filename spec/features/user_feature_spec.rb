552deedece43bc6c4afb06449ea8c3bc1d9e2459
Painted <will.allen@paintedltd.co.uk>
2014-07-14 11:45:22 +0100

    added posted by on posts index with all files

---
+++ spec/features/user_feature_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'users registration/login' do
+
+	it 'can sign up' do
+
+		visit '/users/sign_up'
+		fill_in 'Email', with: 'w@will.com'
+		fill_in 'Password', with: '12345678'
+		fill_in 'Password confirmation', with: '12345678'
+		click_button 'Sign up'
+		expect(page).to have_content 'You have signed up successfully'
+	end
+
+	it 'can sign in' do
+		User.create(email: 'w@will.com', password: '12345678', password_confirmation: '12345678')
+		visit '/users/sign_in'
+		fill_in 'Email', with: 'w@will.com'
+		fill_in 'Password', with: '12345678'
+		click_button 'Sign in'
+		expect(page).to have_content 'Signed in successfully'
+	end
+end
